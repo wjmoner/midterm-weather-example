9cb0775f76903fef2b484369ae4c8a0266c9e681
William J. Moner <wjmoner@gmail.com>
2017-10-19 06:40:50 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,21 +2,23 @@ var weatherData;
 var q = "q=Elon,NC";
 var zip = "zip=15227";
 var units = "units=imperial";
-var appid = "appid=42396ff80468c90bdbb51462e8b1d530";
-var url = "http://api.openweathermap.org/data/2.5/weather?" + q + "&" + units + "&" + appid;
+var appid = "appid=";
+var yourAPIKey; // add your API Key here!
+
+var url = "http://api.openweathermap.org/data/2.5/weather?" + q + "&" + units + "&" + appid + yourAPIKey;
 
 // you can change this around to include other data from the OpenWeatherMaps API
 // the variable structure above makes the code more readable
 
 function preload() {
-	weatherData = loadJSON(url);
+    weatherData = loadJSON(url);
     print(weatherData); // can view weather API object in console to identify keys
 }
 
 function setup() {
-	createCanvas(960, 480);
-	noStroke();
-	fill(255);
+    createCanvas(960, 480);
+    noStroke();
+    fill(255);
 }
 
 function draw() {
